f96a89618fcf9e86cf5e18bbcabe9fe1b3e1d835
Naren Hazareesingh <narenhaz@gmail.com>
2011-03-25 18:45:28 -0500

    Initial Commit

---
+++ Mac OS/Mac_OSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Mac_OSAppDelegate.h
+//  Mac OS
+//
+//  Created by Naren Hazareesingh on 3/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Mac_OSAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Mac OS/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Mac OS
+//
+//  Created by Naren Hazareesingh on 3/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
